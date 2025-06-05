25d90463427d395f8dc33a49e65ceaf4f0b7765c
felixtanza <felixtanzaotienoofficial@gmail.com>
2025-06-06 02:07:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const PORT = process.env.PORT || 5000;
 let visitorCount = 0;
 
 // Middleware
-app.use(cors());
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 // Visitor counter endpoint
 app.get("/api/visitors", (req, res) => {
